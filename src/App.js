cc28d70826d3061be6189b1120ebb6949db0bfd5
Shantanu Dutta <shantanudutta1994@gmail.com>
2021-05-30 12:01:03 +0530

    Added logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Editor from './components/Editor';
-import Toolbar from './components/Toolbar';
 import ExampleDocument from './utils/ExampleDocument';
 
 function App() {
@@ -9,10 +8,18 @@ function App() {
   return (
     <>
       <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#">WYSIWYG Editor</Navbar.Brand>
+        <Navbar.Brand href="#">
+          <img
+            src="./app-icon.png"
+            height="30"
+            width="30"
+            alt="logo"
+            className="d-inline-block align-top"
+          />{' '}
+          WYSIWYG Editor
+        </Navbar.Brand>
       </Navbar>
       <div className="editor h-100 container d-flex flex-column">
-        <Toolbar />
         <Editor document={document} onChange={updateDocument}></Editor>
       </div>
     </>
